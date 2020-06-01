e4337bf0f9a7496ce5405e986df7eb06028b1a6f
IljaLevandovsky <66276814+IljaLevandovsky@users.noreply.github.com>
2020-06-01 23:08:22 +0300

    Файли для сайту

---
+++ myscript.js
@@ -0,0 +1,27 @@
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function () {
+    scrollFunction()
+};
+
+function scrollFunction() {
+    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+        document.getElementById("myBtn").style.display = "block";
+       
+    }
+    else {
+        document.getElementById("myBtn").style.display = "none";
+    }
+}
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+    document.body.scrollTop = 0;
+    document.documentElement.scrollTop = 0;
+}
+// Changing bg color of field
+function myFunction(x) {
+  x.style.background = "burlywood";
+}
+
+function text_alert(){
+    alert("I am an alert box!");
+}
